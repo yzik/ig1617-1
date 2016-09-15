512841539b66aaa6ddba8eb10fc51253f07dda0b
aherrang <aherrang@gmail.com>
2016-09-15 13:03:46 +0200

    Añadidos los recursos de la sesión 01

---
+++ sesion01/src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: aherrang
+ *
+ * Created on 15 de septiembre de 2016, 12:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
