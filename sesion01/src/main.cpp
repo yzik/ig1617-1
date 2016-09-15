f941fd47e8c5e1d6fc4cf792a062583321e4d23a
aherrang <aherrang@gmail.com>
2016-09-15 18:33:53 +0200

    Plantilla inicial de un proyecto

--- sesion01/src/main.cpp
+++ sesion01/src/main.cpp
@@ -1,25 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <stdio.h>
 
-/* 
- * File:   main.cpp
- * Author: aherrang
- *
- * Created on 15 de septiembre de 2016, 12:41
- */
+void initFunc();
+void funReshape(int w, int h);
+void funDisplay();
+void destroyFunc();
 
-#include <cstdlib>
-
-using namespace std;
-
-/*
- * 
- */
 int main(int argc, char** argv) {
-
-    return 0;
+    
+ // Inicializamos GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(50,50);
+    glutCreateWindow("Sesion 1");
+    
+ // Inicializamos GLEW
+    GLenum err = glewInit();
+    if(GLEW_OK != err) {
+        printf("Error: %s\n", glewGetErrorString(err));
+    }
+    printf("Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    
+ // Inicializaciones específicas
+    initFunc();
+    
+ // Configuración CallBacks
+    glutReshapeFunc(funReshape);
+    glutDisplayFunc(funDisplay);
+    
+ // Bucle principal
+    glutMainLoop();
+    
+ // Destrucción de objetos
+    destroyFunc();
+    
+    return(0);
 }
 
+
+void initFunc() {
+    
+}
+
+void destroyFunc() {
+    
+}
+
+void funReshape(int w, int h) {
+    
+}
+
+void funDisplay() {
+    
+ // Borramos el buffer de color
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+ // Dibujamos una tetera con modelo de alambre
+    glColor3f(1.0, 1.0, 0.0);
+    glutWireTeapot(1.0);
+    
+ // Intercambiamos los buffers
+    glutSwapBuffers();
+    
+}
